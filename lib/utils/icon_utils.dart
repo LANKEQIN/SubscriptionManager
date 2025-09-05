813afe6e375969500f97a1490b2f862f82c71a31
ShindoAmane <18376668690@163.com>
2025-09-05 21:45:02 +0800

    优化代码

--- lib/utils/icon_utils.dart
+++ lib/utils/icon_utils.dart
@@ -3,17 +3,7 @@ import 'package:flutter/material.dart';
 class IconUtils {
   /// 从订阅图标字符串创建图标组件
   static Widget buildSubscriptionIcon(BuildContext context, String? iconCode, {double size = 24}) {
-    IconData iconData;
-    
-    if (iconCode == null) {
-      iconData = Icons.subscriptions_outlined;
-    } else {
-      try {
-        iconData = IconData(int.parse(iconCode), fontFamily: 'MaterialIcons');
-      } catch (e) {
-        iconData = Icons.subscriptions_outlined;
-      }
-    }
+    final IconData iconData = getIconData(iconCode);
     
     return Icon(
       iconData,
@@ -24,14 +14,14 @@ class IconUtils {
   
   /// 从订阅图标字符串获取IconData
   static IconData getIconData(String? iconCode) {
+    // 使用预定义的常量图标，避免运行时动态创建IconData
+    // 这里可以扩展为映射表，将特定的iconCode映射到预定义的常量图标
     if (iconCode == null) {
       return Icons.subscriptions_outlined;
     }
     
-    try {
-      return IconData(int.parse(iconCode), fontFamily: 'MaterialIcons');
-    } catch (e) {
-      return Icons.subscriptions_outlined;
-    }
+    // 由于tree shaking要求使用常量IconData，这里返回一个默认图标
+    // 如果需要支持特定图标，应该预先定义所有可能的图标常量
+    return Icons.subscriptions_outlined;
   }
 }
